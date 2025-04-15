13bc2e8c561ea20281642a1c826658b4ba635ec9
Guilherme Borba <guifborba@hotmail.com>
2025-04-15 20:10:59 -0300

    gitignore

--- disciplinas/models.py
+++ disciplinas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Disciplina(models.Model):
+    nome = models.CharField(max_length=50)
+    professor = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=500)
+
+    def _str_(self):
+        return self.nome
+
--- disciplinas/views.py
+++ disciplinas/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Disciplina
 
 @login_required(login_url='login')
 def index(request):
 
-    return HttpResponse ('Você conseguiu')
+    disciplinas = Disciplina.objects.all
