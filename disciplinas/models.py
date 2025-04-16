6328878927b1df9c9173b9f86db9bf3f44cf2dc0
Guilherme Borba <guifborba@hotmail.com>
2025-04-16 15:12:54 -0300

    criacao_model_avaliacao

--- disciplinas/models.py
+++ disciplinas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Disciplinas(models.Model):
     nome = models.CharField(max_length=50)
@@ -8,3 +9,12 @@ class Disciplinas(models.Model):
     def _str_(self):
         return self.nome
 
+class Avaliacao(models.Model):
+    disciplina = models.ForeignKey(Disciplinas, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    nota = models.PositiveSmallIntegerField()
+    comentario = models.CharField(max_length=500)
+    data = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('disciplina', 'user')
